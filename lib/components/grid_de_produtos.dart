57842abc355c92ba147bf9d845569b670ad8b79b
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-08-08 16:52:11 -0300

    adicionando grid de produtos

---
+++ lib/components/grid_de_produtos.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/lista_produtos.dart';
+import '../models/produtos.dart';
+import 'item_produtos.dart';
+
+class GridDeProdutos extends StatelessWidget {
+  const GridDeProdutos({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ListaProdutos provide = Provider.of<ListaProdutos>(context);
+    final List<Produtos> produtos = provide.itensProdutos;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10),
+      itemCount: produtos.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        mainAxisSpacing: 10,
+        crossAxisSpacing: 10,
+        childAspectRatio: 3 / 2,
+      ),
+      itemBuilder: (cxt, indice) => ChangeNotifierProvider.value(
+        value: produtos[indice],
+        child: const ItemProdutos(),
+      ),
+    );
+  }
+}
