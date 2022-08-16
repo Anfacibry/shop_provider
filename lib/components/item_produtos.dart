e728112f4bc352f203734997e3a4d7050750ce46
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-08-16 10:57:05 -0300

    atualizando o components item de produtos para fazer o consumos de dados com Consumer()

--- lib/components/item_produtos.dart
+++ lib/components/item_produtos.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop_provider/models/produtos.dart';
+
 import 'package:shop_provider/utils/rotas_app.dart';
 
 class ItemProdutos extends StatelessWidget {
-  final Produtos produtos;
-  const ItemProdutos({required this.produtos, Key? key}) : super(key: key);
+  const ItemProdutos({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Produtos produtos = Provider.of<Produtos>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black54,
-          leading: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.favorite,
-              color: Theme.of(context).colorScheme.secondary,
+          leading: Consumer<Produtos>(
+            builder: (cntx, produtos, child) => IconButton(
+              onPressed: () {
+                produtos.verificacaoFavorito();
+              },
+              icon: Icon(
+                produtos.eFavorito ? Icons.favorite : Icons.favorite_border,
+                color: Theme.of(context).colorScheme.secondary,
+              ),
             ),
           ),
           title: Text(
