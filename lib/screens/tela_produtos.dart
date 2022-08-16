9a5104e1c90a287e5d9b123f93eb1092691b6ecc
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-08-16 16:12:47 -0300

    atualizando os favoritos para aparecerem na tela quando selecionando

--- lib/screens/tela_produtos.dart
+++ lib/screens/tela_produtos.dart
@@ -2,17 +2,53 @@ import 'package:flutter/material.dart';
 
 import '../components/grid_de_produtos.dart';
 
-class TelaProdutos extends StatelessWidget {
+enum FiltrandoOpcao {
+  favoritos,
+  todos,
+}
+
+class TelaProdutos extends StatefulWidget {
   const TelaProdutos({Key? key}) : super(key: key);
 
+  @override
+  State<TelaProdutos> createState() => _TelaProdutosState();
+}
+
+class _TelaProdutosState extends State<TelaProdutos> {
+  bool _selecaoFavorito = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Produtos"),
         centerTitle: true,
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (contextPop) => [
+              const PopupMenuItem(
+                value: FiltrandoOpcao.favoritos,
+                child: Text("Favoritos"),
+              ),
+              const PopupMenuItem(
+                value: FiltrandoOpcao.todos,
+                child: Text("Todos"),
+              ),
+            ],
+            onSelected: (selecao) {
+              setState(() {
+                if (selecao == FiltrandoOpcao.favoritos) {
+                  _selecaoFavorito = true;
+                } else {
+                  _selecaoFavorito = false;
+                }
+              });
+            },
+          ),
+        ],
       ),
-      body: const GridDeProdutos(),
+      body: GridDeProdutos(
+        selecaoFavorito: _selecaoFavorito,
+      ),
     );
   }
 }
