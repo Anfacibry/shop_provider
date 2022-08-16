134d590d2bfc9a0e832a7afac637b5bbef7573f3
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-08-16 16:11:18 -0300

    adicionando itens para o carrinho

---
+++ lib/models/itens_carrinho.dart
@@ -0,0 +1,15 @@
+class ItemCarrinho {
+  final String id;
+  final String idProduto;
+  final String produtoTitulo;
+  final int quantidade;
+  final double preco;
+
+  ItemCarrinho({
+    required this.id,
+    required this.idProduto,
+    required this.produtoTitulo,
+    required this.quantidade,
+    required this.preco,
+  });
+}
