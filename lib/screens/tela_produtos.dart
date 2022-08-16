93d1d30e650f21b14513387839b53dd4f9398e12
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-08-16 10:58:53 -0300

    atualizando a tela de produtos para otmizar codigo

--- lib/screens/tela_produtos.dart
+++ lib/screens/tela_produtos.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:shop_provider/components/item_produtos.dart';
-import 'package:shop_provider/data/dados_produtos.dart';
-import 'package:shop_provider/models/produtos.dart';
+
+import '../components/grid_de_produtos.dart';
 
 class TelaProdutos extends StatelessWidget {
-  final List<Produtos> produtos = DadosDeProdutos().dadosProdutos;
-  TelaProdutos({Key? key}) : super(key: key);
+  const TelaProdutos({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +12,7 @@ class TelaProdutos extends StatelessWidget {
         title: const Text("Produtos"),
         centerTitle: true,
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: produtos.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisSpacing: 10,
-          crossAxisSpacing: 10,
-          childAspectRatio: 3 / 2,
-        ),
-        itemBuilder: (cxt, indice) => ItemProdutos(
-          produtos: produtos[indice],
-        ),
-      ),
+      body: const GridDeProdutos(),
     );
   }
 }
