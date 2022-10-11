3c936e1fbae8e940d5688017fc2dabd9e8adcdff
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-10-11 15:53:52 -0300

    adicionando pasta de constantes

---
+++ lib/utils/constantes.dart
@@ -0,0 +1,6 @@
+class Urls {
+  static const String urlProdutos =
+      "https://shop-provider-949c2-default-rtdb.firebaseio.com/produtos";
+  static const String urlOrdem =
+      "https://shop-provider-949c2-default-rtdb.firebaseio.com/ordem";
+}
