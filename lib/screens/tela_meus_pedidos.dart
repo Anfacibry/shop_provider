9ef4b80f8fd9d4c8fdad37132fd1bf78bfefae74
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-10-11 15:54:33 -0300

    atualiando tela de pedidos para consumir dados da nuvem

--- lib/screens/tela_meus_pedidos.dart
+++ lib/screens/tela_meus_pedidos.dart
@@ -7,20 +7,45 @@ import 'package:shop_provider/provider/lista_ordenada.dart';
 class MeusPedidos extends StatelessWidget {
   const MeusPedidos({Key? key}) : super(key: key);
 
+  Future<void> carregandoPedidos(BuildContext context) async {
+    return await Provider.of<ListaOrdenada>(
+      context,
+      listen: false,
+    ).pegandoOrdem();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final ListaOrdenada listaOrdenada = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Meus pedidos"),
         centerTitle: true,
       ),
       drawer: const IconeDrawer(),
-      body: ListView.builder(
-        itemCount: listaOrdenada.tamanhoListaOrdem,
-        itemBuilder: (context, indice) => CardMeusPedidos(
-          ordem: listaOrdenada.itens[indice],
-        ),
+      body: FutureBuilder(
+        future: Provider.of<ListaOrdenada>(
+          context,
+          listen: false,
+        ).pegandoOrdem(),
+        builder: (contx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return Consumer<ListaOrdenada>(
+              builder: (contx, listaOrdenada, child) => RefreshIndicator(
+                onRefresh: () => carregandoPedidos(context),
+                child: ListView.builder(
+                  itemCount: listaOrdenada.tamanhoListaOrdem,
+                  itemBuilder: (context, indice) => CardMeusPedidos(
+                    ordem: listaOrdenada.itens[indice],
+                  ),
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
