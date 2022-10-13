b2d5c62cda221f212f442bdae00ba0b01f9be0ac
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-10-13 16:13:18 -0300

    Criando tela de login

---
+++ lib/components/aut_form_login.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+
+enum ModoLogin { login, registro }
+
+class AutFormLogin extends StatefulWidget {
+  const AutFormLogin({super.key});
+
+  @override
+  State<AutFormLogin> createState() => _AutFormLoginState();
+}
+
+class _AutFormLoginState extends State<AutFormLogin> {
+  ModoLogin statusModoLogin = ModoLogin.login;
+  TextEditingController senhaController = TextEditingController();
+
+  bool get login => statusModoLogin == ModoLogin.login;
+  bool get registro => statusModoLogin == ModoLogin.registro;
+
+  final keyForm = GlobalKey<FormState>();
+  bool carregando = false;
+
+  Map<String, String> dadosLogin = {
+    "email": "",
+    "senha": "",
+  };
+  void alterandoCarregando() {
+    final validacaoKey = keyForm.currentState?.validate() ?? false;
+
+    if (!validacaoKey) {
+      return;
+    }
+
+    setState(() {
+      carregando = true;
+      keyForm.currentState?.save();
+      carregando = false;
+    });
+  }
+
+  void loginOuRegistro() {
+    setState(() {
+      if (login) {
+        statusModoLogin = ModoLogin.registro;
+      } else {
+        statusModoLogin = ModoLogin.login;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tamanhoTela = MediaQuery.of(context).size;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      margin: const EdgeInsets.only(right: 10),
+      child: SizedBox(
+        height: login ? 350 : 400,
+        width: tamanhoTela.width * 0.80,
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Form(
+            key: keyForm,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  onSaved: (emailPego) => dadosLogin["email"] = emailPego ?? "",
+                  validator: (email) {
+                    final emailValidar = email ?? "";
+                    if (emailValidar.isEmpty && !emailValidar.contains("@")) {
+                      return "Digite um email valido";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: "Senha",
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  controller: senhaController,
+                  obscureText: true,
+                  onSaved: (senhaPego) => dadosLogin["senha"] = senhaPego ?? "",
+                  validator: (senhPegaVadidar) {
+                    final senhaValidar = senhPegaVadidar ?? "";
+                    if (senhaValidar.isEmpty && senhaValidar.length < 6) {
+                      return "Digite uma senha valida";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                if (registro)
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      labelText: "Confirmar senha",
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: login
+                        ? null
+                        : (senhaPega) {
+                            if (senhaPega != senhaController.text) {
+                              return "Por favor, digite a mesma senha do campo de senha";
+                            } else {
+                              return null;
+                            }
+                          },
+                    obscureText: true,
+                  ),
+                const Padding(padding: EdgeInsets.only(bottom: 20)),
+                carregando
+                    ? const CircularProgressIndicator()
+                    : ElevatedButton(
+                        onPressed: alterandoCarregando,
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 40)),
+                        child: Text(login ? "Login" : "Registrar"),
+                      ),
+                const Spacer(
+                  flex: 5,
+                ),
+                TextButton(
+                    onPressed: loginOuRegistro,
+                    child: Text(
+                      login
+                          ? "Deseja registrar uma nova conta?"
+                          : "Já possui login?",
+                    ))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
